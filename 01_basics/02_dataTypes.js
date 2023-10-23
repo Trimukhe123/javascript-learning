1435077b06e3d43f755286892b7d75f004ce7242
Trimukhe123 <143921575+Trimukhe123@users.noreply.github.com>
2023-10-23 15:59:18 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Trimukhe"
+let age = 22
+let isLoggedIn = true
+let state;
+let big = 1231526135716172729n  // bigint
+
+
+// number  // 2 , 2.3
+// bigint // for large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof name);
+console.log(typeof state); // undefined
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof big);
