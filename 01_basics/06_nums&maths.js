480b4c3cf9d4071301ec8380246947a9c3624add
Trimukhe123 <143921575+Trimukhe123@users.noreply.github.com>
2023-10-23 16:07:57 +0000

    maths and numbers

---
+++ 01_basics/06_nums&maths.js
@@ -0,0 +1,25 @@
+const score = 200
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const hunderds = 1000000
+console.log(hunderds.toLocaleString('en-IN'));
+
+
+
+// ************** Maths *********************
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.pow(2,3));
+console.log(Math.max(1,2,3,0));
+console.log(Math.min(0,100,-2));
+console.log(Math.random());
