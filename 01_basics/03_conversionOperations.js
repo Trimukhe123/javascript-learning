079b8586c019961511963eb97aee06e686155e3d
Trimukhe123 <143921575+Trimukhe123@users.noreply.github.com>
2023-10-23 16:01:08 +0000

    Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,47 @@
+// let score = 22
+// let score1 = "22"
+// let score2 = true
+
+// console.log(typeof score)
+// console.log(typeof(score))
+// console.log(typeof score1)
+
+// let valueInNumber = Number(score1)
+// let valueInNumber2 = Number(score2)
+
+// console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
+
+// "22" => 22
+// "22abc" => Nan (not an number)
+// true => 1; false =>0
+// 1 => true; 0 => false
+// "" => false
+// "jt" => true
+
+
+// **************************** Opeartions **********************
+
+
+let value = 2
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3); // power i.e 2 power 3 = 8
+
+let str1 = "Jyotiba"
+let str2 = " Trimukhe"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // we found starting as string so everything from here will be treated as string
+console.log(1 + 2 + "2"); // first it will add 1+2=3 net we found string so output will be 32
